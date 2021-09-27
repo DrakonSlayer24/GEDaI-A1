1d22ed63937431f79ec9fcd061795845a69f206c
Alvin Ng <Alvin.Ng@ontariotechu.net>
2021-09-26 21:37:12 -0400

    Added Power Up
    added power up boost object

---
+++ Assets/Scripts/PlayerBoost.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBoost : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float BoostDistance;
+    private float Count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision PUCol)
+    {
+        Boost PU = PUCol.gameObject.GetComponent<Boost>();
+        if (PU)
+        {
+            Destroy(PUCol.gameObject);
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + BoostDistance);
+        }
+    }
+}
